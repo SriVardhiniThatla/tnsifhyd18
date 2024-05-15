4d80f6f074e3b7f97e41c8cb277634db47c5deef
SriVardhiniThatla <168456401+SriVardhiniThatla@users.noreply.github.com>
2024-05-15 23:50:34 +0530

    Add files via upload

---
+++ PolymorphismLanguage1ExtendsMOverriding.java
@@ -0,0 +1,9 @@
+package com.sri.vardhini;
+
+public class PolymorphismLanguage1ExtendsMOverriding extends PolymorphismLanguage1MOverriding{
+
+	@Override
+	  public void displayInfo() {
+	    System.out.println("Java Programming Language");
+	  }
+}
