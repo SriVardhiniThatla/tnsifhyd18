abd10dcf297ba55c84e61ba83a500ab0bfadefa2
SriVardhiniThatla <168456401+SriVardhiniThatla@users.noreply.github.com>
2024-05-15 23:55:37 +0530

    Add files via upload

---
+++ TestThisStatic.java
@@ -0,0 +1,11 @@
+package com.sri.vardhini;
+
+public class TestThisStatic {
+	public static void main(String args[]) 
+	{
+	 //Static method is being called using the name of the class
+	StaticDemo.callme();
+	 //Static variable is accessed with the class name
+	System.out.println("The value of b = " + StaticDemo.a);
+	}
+}
