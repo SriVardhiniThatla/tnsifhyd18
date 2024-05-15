aa2871e6c7b91d80f32e2efcbf2645d9ecc1e145
SriVardhiniThatla <168456401+SriVardhiniThatla@users.noreply.github.com>
2024-05-15 13:29:31 +0530

    Add files via upload

---
+++ Approach2.java
@@ -0,0 +1,12 @@
+package com.sri.vardhini;
+
+public class Approach2 {
+	String batsman="virat kohli";
+	static String bowlername="shami";
+	void display() {
+		System.out.println("great cricketer");
+	}
+	static String display1() {
+		return "good batsman";
+	}
+}
