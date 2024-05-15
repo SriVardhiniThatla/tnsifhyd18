fb91306622971c5a2808e77445db220e80109683
SriVardhiniThatla <168456401+SriVardhiniThatla@users.noreply.github.com>
2024-05-15 23:52:31 +0530

    Add files via upload

---
+++ PolymorphismMOverloadMain.java
@@ -0,0 +1,12 @@
+package com.sri.vardhini;
+
+public class PolymorphismMOverloadMain {
+	public static void main(String[] args) {
+		PolymorphismMOverload m1 = new PolymorphismMOverload();
+		m1.display();
+		
+		System.out.println("\n");
+		
+		m1.display('#');
+	}
+}
