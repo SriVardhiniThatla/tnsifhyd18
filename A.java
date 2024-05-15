b865a5adc3987b40a007f6522044eca9ff768846
SriVardhiniThatla <168456401+SriVardhiniThatla@users.noreply.github.com>
2024-05-15 13:23:03 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,10 @@
+package com.sri.vardhini;
+
+public class A {
+void display() {
+	System.out.println("contents of class A");
+}
+void display(int a) {
+	System.out.println("contains paramter:"+a);
+}
+}
