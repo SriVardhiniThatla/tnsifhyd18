5b69a52a2c0753ef923fc1945728a4e161c4d09f
SriVardhiniThatla <168456401+SriVardhiniThatla@users.noreply.github.com>
2024-05-15 13:28:38 +0530

    Add files via upload

---
+++ Access.java
@@ -0,0 +1,11 @@
+package com.sri.vardhini;
+  public class Access {
+		public static void main(String[]args) {
+			Approach2 a1=new Approach2();
+			System.out.println(a1.batsman);
+			a1.display();
+			System.out.println(Approach2.bowlername);
+			System.out.println(Approach2.display1());
+		}
+	}
+
