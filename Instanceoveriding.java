b942822d11852ffc150a4b9a8c50ab992ad1d3a4
SriVardhiniThatla <168456401+SriVardhiniThatla@users.noreply.github.com>
2024-05-15 23:38:49 +0530

    Add files via upload

---
+++ Instanceoveriding.java
@@ -0,0 +1,13 @@
+package com.sri.vardhini;
+
+public class Instanceoveriding {
+
+	public static void main(String[] args) {
+		//Parent p1 = new Parent();
+		//p1.show();
+		
+		Parent p2 = new Child();
+		p2.show();
+		
+	}
+}
