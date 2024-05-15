7384309878adf122c01c1f5490ce9ab2d332f9d4
SriVardhiniThatla <168456401+SriVardhiniThatla@users.noreply.github.com>
2024-05-15 23:50:09 +0530

    Add files via upload

---
+++ Parent.java
@@ -0,0 +1,13 @@
+package com.sri.vardhini;
+
+public class Parent {
+	//Instance method
+		public void show() {
+			System.out.println("Parents show() is called");
+		}
+		
+		//Static method
+		public static void print() {
+			System.out.println("Parent static print() is called");
+		}
+}
