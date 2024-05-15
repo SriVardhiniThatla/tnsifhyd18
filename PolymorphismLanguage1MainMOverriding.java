f101ae29101f4ce9442de625585d197d5c376feb
SriVardhiniThatla <168456401+SriVardhiniThatla@users.noreply.github.com>
2024-05-15 23:50:55 +0530

    Add files via upload

---
+++ PolymorphismLanguage1MainMOverriding.java
@@ -0,0 +1,12 @@
+package com.sri.vardhini;
+
+public class PolymorphismLanguage1MainMOverriding {
+public static void main(String[] args) {
+		
+		PolymorphismLanguage1ExtendsMOverriding p1 = new PolymorphismLanguage1ExtendsMOverriding();
+		p1.displayInfo();
+		
+		PolymorphismLanguage1MOverriding p2 = new PolymorphismLanguage1MOverriding();
+		p2.displayInfo();
+	}
+}
