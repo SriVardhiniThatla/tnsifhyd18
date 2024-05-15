02139e6a5a1eb99cd94907c7fd0754787282e292
SriVardhiniThatla <168456401+SriVardhiniThatla@users.noreply.github.com>
2024-05-15 23:49:48 +0530

    Add files via upload

---
+++ NoConstructorMain.java
@@ -0,0 +1,9 @@
+package com.sri.vardhini;
+
+public class NoConstructorMain {
+	public static void main(String[] args) {
+		NoConstructor n1 = new NoConstructor();
+		System.out.println(n1.getBrand());
+	}
+
+}
